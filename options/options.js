77b124a16a40553535f9de73433fe239ee78750b
Crowdbotics <team@crowdbotics.com>
2021-08-06 14:27:04 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "tstdanielblank_dev_22855Identifier",
+  url: "https://tstdanielblank_dev_22855Identifier.botics.co",
+  api: "https://tstdanielblank_dev_22855Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
